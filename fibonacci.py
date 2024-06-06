e95333ce916c842c437a097f445dfa5f2296d1dd
PAB <baidooprinceaikins@gmail.com>
2024-06-06 05:25:37 +0000

    Updated fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -4,6 +4,18 @@ num1 = 0
 num2 = 1
 print("Fibonacci sequence:")
 while len(fib) <= num_of_fib:
+    num1, num2 = num2, (num1 + num2)
+    fib.append(num2)
+for i in fib:
+    print(i)
+
+# ANALTERNATIVE APPROACH
+num_of_fib = int(input("Enter the number of fibonacci numbers you want: "))
+fib = [0, 1]
+num1 = 0
+num2 = 1
+print("Fibonacci sequence:")
+while len(fib) <= num_of_fib:
     num2 = num1 + num2
     num1 = num2 - num1
     fib.append(num2)
